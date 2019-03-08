423418a3d20bfde256cc1d11c5f38cb6a812d4aa
Jingi Kim <copyx00@gmail.com>
2019-03-08 11:50:47 +0900

    chore: 배포 잘 되는지 다시 테스트

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Admin's Vue.js App"/>
+    <HelloWorld msg="Welcome to Fomes Admin's Vue.js App"/>
   </div>
 </template>
 
