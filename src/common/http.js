f26f053f96ff5e81d54d6bebe542a1d1db430d5b
Jake Kim and Yena Kim <pair+copyx00+yenarue@gmail.com>
2019-03-14 13:35:15 +0900

    fixed: ES6 문법 적용 (default export)

--- src/common/http.js
+++ src/common/http.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import config from '../../config';
 
-module.exports = axios.create({
+export default axios.create({
   baseURL: config.serverBaseUrl,
-  withCredentials: true,
 });
