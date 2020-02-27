0b966854083c3f5bf227277b33fcdecce7067690
yenarue <yenarue@gmail.com>
2020-02-27 21:35:37 +0900

    chore: 슬랙 메세지에 new line 추가
    https://trello.com/c/1taOBWyS

--- .circleci/delivery-to-slack.sh
+++ .circleci/delivery-to-slack.sh
@@ -11,8 +11,10 @@ fi
 echo "version : $VERSION"
 ### end of getting version
 
+NEW_LINE=$'\n'
+
 curl -F channel="#_general" \
-    -F text="운영페이지 $VERSION 버전이 릴리즈 되었습니다!\n:point_right: <https://fomes-admin-frontend.herokuapp.com|운영페이지 확인하러 가기>" \
+    -F text="운영페이지 $VERSION 버전이 릴리즈 되었습니다!${NEW_LINE}:point_right::skin-tone-2: <https://fomes-admin-frontend.herokuapp.com|운영페이지 확인하러 가기>" \
     -F token=$SLACK_BOT_TOKEN \
     -F as_user=true \
     https://slack.com/api/chat.postMessage
