44d9e2b80eb6ac477fe0ab8660edbb5a60a24ca5
Jason Ryu and Yena Kim <pair+sryu99+yenarue@gmail.com>
2020-08-04 17:51:15 +0900

    feat: 수상 수정 팝업 - 수상 관련 정보 필드 추가
    - 유저 아이디, 닉네임, 보상 지급 유형
    - 리팩토링 - 불필요한 로직 제거
    - 밸리데이션 - 필수 필드 추가
    
    https://trello.com/c/1gZu0jBh

--- src/award-records/components/AwardRecordDetailForm.vue
+++ src/award-records/components/AwardRecordDetailForm.vue
@@ -1,31 +1,61 @@
 <template>
   <div class="modal-card award-record-detail-container" style="width: auto">
     <header class="modal-card-head">
-      <p class="modal-card-title">수상자 정보 수정하기</p>
+      <p class="modal-card-title">수상 정보 수정하기</p>
     </header>
     <div class="modal-card-body">
       <section style="width: 100%">
-        <div class="columns">
-          <div class="column is-one-third">
-            <b-field label="보상 유형">
-              <b-select v-model="rewardType" expanded>
+        <b-field label="userId" horizontal>
+          {{ awardRecord.userId }}
+        </b-field>
+
+        <b-field label="닉네임" horizontal>
+          {{ awardRecord.nickName }}
+        </b-field>
+
+        <div class="columns is-multiline">
+          <div class="column">
+            <b-field >
+              <template slot="label">
+                <span class="has-text-danger">*</span> 보상 유형
+              </template>
+              <b-select v-model="awardRecord.typeCode" expanded required>
                 <option v-for="option in options.rewardTypes" :key="option.key" :value="option.value.typeCode">
                   {{ option.value.title }}
                 </option>
               </b-select>
             </b-field>
           </div>
-          <div class="column is-one-third">
+          <div class="column">
             <b-field>
               <template slot="label">
                 <span class="has-text-danger">*</span> 보상 설명
               </template>
-              <b-input v-model="awardRecord.reward.description"></b-input>
+              <b-input v-model="awardRecord.reward.description" required></b-input>
             </b-field>
           </div>
-          <div class="column is-one-third">
-            <b-field label="보상 가격">
-              <b-input v-model="awardRecord.reward.price"></b-input>
+          <div class="column">
+            <b-field>
+              <template slot="label">
+                <span class="has-text-danger">*</span> 보상 가격
+              </template>
+              <b-input v-model="awardRecord.reward.price" required></b-input>
+            </b-field>
+          </div>
+          <div class="column">
+            <b-field>
+              <template slot="label">
+                <span class="has-text-danger">*</span> 보상 지급 유형
+              </template>
+              <div>
+                <b-select v-model="awardRecord.reward.paymentType" required>
+                  <option v-for="paymentType in options.paymentTypes"
+                          :key="paymentType.key"
+                          :value="paymentType.value">
+                    {{ paymentType.text }}
+                  </option>
+                </b-select>
+              </div>
             </b-field>
           </div>
         </div>
@@ -47,13 +77,13 @@
     watch:{
       'rewardList':{
         handler(value){
-          this.rewardType = value[0].typeCode ? value[0].typeCode : 9000;
           this.options.rewardList = value;
         },
         deep:true
       },
-      'rewardType':{
+      'awardRecord.typeCode':{
         handler(value){
+          console.log('watch', value);
           this.rewardList.forEach((e)=>{
             if(e.typeCode === value){
               this.awardRecord.reward.description = e.content;
@@ -68,7 +98,6 @@
     },
     data() {
       return {
-        rewardType:9000,
         awardRecord:{
           _id: '',
           nickName:'',
@@ -84,21 +113,27 @@
     },
     mounted() {
       this.awardRecord = Object.assign({}, this.value);
-      this.rewardType = this.awardRecord.typeCode;
     },
     methods: {
       validate() {
-        return this.awardRecord.reward.description.trim().length > 0;
+        return this.awardRecord.reward.description.trim().length > 0
+         && this.awardRecord.reward.price.toString().trim().length > 0
+         && (!!this.awardRecord.reward.paymentType && this.awardRecord.reward.paymentType.length > 0)
       },
       update(){
-        if(this.validate()){
-          httpRequest.put('/api/award-records/'+this.awardRecord._id, this.awardRecord).then(res=>{
+        if (!this.validate()) {
+          this.$root.showErrorToast('필수 값들을 모두 입력해주세요', {});
+          return;
+        }
+
+        httpRequest.put('/api/award-records/' + this.awardRecord._id, this.awardRecord)
+          .then(res => {
             this.$root.showSuccessToast('수상 내역을 정상적으로 수정하였습니다.');
             this.$emit('close', true);
-          }).catch(err=>{
-            this.$root.showErrorToast('수상 내역 수정에 실패하였습니다.', err);
           })
-        }
+          .catch(err => {
+            this.$root.showErrorToast('수상 내역 수정에 실패하였습니다.', err);
+          });
       },
       //리워드 관련 임시 처리 (추후 앱 크리티컬 업데이트 시 아래 내용 삭제 필요)
       getRewardType(typeCode) {
