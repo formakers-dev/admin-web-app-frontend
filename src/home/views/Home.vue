c869168b0a061170cd13d6a226d99a516bc869dd
yenarue <yenarue@gmail.com>
2020-08-20 14:34:33 +0900

    style: 운영페이지 메인화면 디자인 업데이트
    https://trello.com/c/vrmvEaA1

--- src/home/views/Home.vue
+++ src/home/views/Home.vue
@@ -3,10 +3,10 @@
     <div class="hero-body">
       <div class="container">
         <h1 class="title" style="font-size: 100px">
-          👩🏻‍🎨👩🏻‍💻👨🏻‍💻👨🏻‍💻
+          <img src="../../assets/logo_kr.png"/>
         </h1>
         <h2 class="subtitle" style="font-size: 70px">
-          <b style="color: #00BFBA">ForMakers Admin</b>
+          <b style="color: #00BFBA">Admin</b>
         </h2>
       </div>
     </div>
