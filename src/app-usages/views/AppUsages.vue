5f1e2a960b70cbdf15400f0bea3a5ecbf15cd2d7
yenarue <yenarue@gmail.com>
2020-08-20 20:35:56 +0900

    feat: 게임 플레이 시간 조회 메뉴 진입시 로딩뷰 추가
    - feat: 상세 내용 row 데이터로 임시 추가
    
    https://trello.com/c/TDuThApw

--- src/app-usages/views/AppUsages.vue
+++ src/app-usages/views/AppUsages.vue
@@ -2,62 +2,74 @@
   <div>
     <h1 class="title">게임 플레이 시간 조회</h1>
 
-    <b-table
-      ref="table"
-      :data="appUsageList"
-      :bordered="false"
-      :hoverable="true"
-      :show-detail-icon="true"
-      default-sort="date"
-      default-sort-direction="desc"
-      detailed
-      selectable
-    >
-      <template slot-scope="props">
-        <b-table-column field="userId" label="유저ID" searchable>
-          <div style="font-size: 0.8rem">
-          {{ props.row.userId }}
+    <div class="content">
+      <b-loading :is-full-page="false" :active.sync="isLoading"></b-loading>
+
+      <b-table
+        ref="table"
+        :data="appUsageList"
+        :bordered="false"
+        :hoverable="true"
+        :show-detail-icon="true"
+        default-sort="date"
+        default-sort-direction="desc"
+        detail-key="_id"
+        detailed
+        selectable
+      >
+        <template slot-scope="props">
+          <b-table-column field="userId" label="유저ID" searchable>
+            <div style="font-size: 0.8rem">
+              {{ props.row.userId }}
+            </div>
+          </b-table-column>
+
+          <b-table-column field="genderString" label="성별" searchable>
+            {{ props.row.genderString }}
+          </b-table-column>
+
+          <b-table-column field="ageRange" label="나이대" searchable>
+            {{ props.row.ageRange }}
+          </b-table-column>
+
+          <b-table-column field="appName" label="앱" searchable>
+            <div style="font-size: 0.8rem">
+              <img :src="props.row.iconUrl" width="30"/>
+              {{ props.row.appName }}
+            </div>
+          </b-table-column>
+
+          <b-table-column field="date" label="날짜" searchable sortable>
+            {{ convertDateFormat(props.row.date) }}
+          </b-table-column>
+
+          <b-table-column field="totalUsedTime" label="플레이 시간" sortable>
+            {{ convertDuration(props.row.totalUsedTime) }}
+          </b-table-column>
+        </template>
+
+        <template slot="detail" slot-scope="props">
+          <div class="box">
+            {{ props.row }}
           </div>
-        </b-table-column>
+        </template>
 
-        <b-table-column field="genderString" label="성별" searchable>
-          {{ props.row.genderString }}
-        </b-table-column>
+        <template slot="empty">
+          <section v-if="!isLoading" class="section">
+            <div class="content has-text-grey has-text-centered">
+              <p>
+                <b-icon
+                  icon="emoticon-sad"
+                  size="is-large">
+                </b-icon>
+              </p>
+              <p>데이터가 없습니다</p>
+            </div>
+          </section>
+        </template>
+      </b-table>
 
-        <b-table-column field="ageRange" label="나이대" searchable>
-          {{ props.row.ageRange }}
-        </b-table-column>
-
-        <b-table-column field="appName" label="앱" searchable>
-          <div style="font-size: 0.8rem">
-          <img :src="props.row.iconUrl" width="30"/>
-            {{ props.row.appName }}</div>
-        </b-table-column>
-
-        <b-table-column field="date" label="날짜" searchable sortable>
-          {{ convertDateFormat(props.row.date) }}
-        </b-table-column>
-
-        <b-table-column field="totalUsedTime" label="플레이 시간" sortable>
-          {{ convertDuration(props.row.totalUsedTime) }}
-        </b-table-column>
-      </template>
-
-      <template slot="empty">
-        <section v-if="!$root.isLoading" class="section">
-          <div class="content has-text-grey has-text-centered">
-            <p>
-              <b-icon
-                icon="emoticon-sad"
-                size="is-large">
-              </b-icon>
-            </p>
-            <p>데이터가 없습니다</p>
-          </div>
-        </section>
-      </template>
-    </b-table>
-
+    </div>
   </div>
 </template>
 
@@ -72,9 +84,11 @@
     data() {
       return {
         appUsageList: [],
+        isLoading: true,
       };
     },
     created() {
+      this.isLoading = true;
       this.loadAppUsages();
     },
     methods: {
@@ -87,8 +101,10 @@
               appUsage.genderString = usersUtil.convertGenderToString(appUsage.gender);
               return appUsage
             });
+            this.isLoading = false;
           })
           .catch((err) => {
+            this.isLoading = false;
             this.$buefy.toast.open({
               message: '앱 사용 정보를 불러오는데 실패하였습니다.',
               type: 'is-danger',
