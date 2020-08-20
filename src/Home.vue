cea3d0cc336c06f7b582806c4626c93a1a388d71
yenarue <yenarue@gmail.com>
2020-08-20 19:58:43 +0900

    chore: 메인화면 - 릴리즈노트 링크 추가

--- src/Home.vue
+++ src/Home.vue
@@ -8,6 +8,9 @@
         <h2 class="subtitle" style="font-size: 70px">
           <b style="color: #00BFBA">Admin</b>
         </h2>
+        <a href="https://www.notion.so/formakers/0x05_-ce8801b75a924148b983b0aad578eb89" target="_blank">
+          운영페이지 업데이트 내용이 궁금하다면?
+        </a>
       </div>
     </div>
   </section>
