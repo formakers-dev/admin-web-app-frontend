9bd4b0d72553932237124c428952d017616a3949
yenarue <yenarue@gmail.com>
2020-10-23 00:07:38 +0900

    feat: 응답 조회 화면 1차 추가
    https://trello.com/c/G0uFrIUr

---
+++ src/beta-tests/views/TestFeedbackAggregation.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <div class="head">
+      <div class="contents">
+        <img v-if="betaTest.iconImageUrl" style="width: 70px" :src="betaTest.iconImageUrl"/>
+        <div>
+          <h1 class="title" style="margin-top: 10px; display: inline">
+            {{ this.betaTest.title }}
+            - {{ mission.title }}
+            <div class="has-text-primary" style="display: inline">응답 집계</div>
+          </h1>
+          <b-tooltip v-if="isTargetToFomesMembers"
+                     position="is-right"
+                     sizr="is-small"
+                     style="width: inherit;"
+                     label="해당 테스트가 포메스 관리자들에게만 보여지는 모드입니다."
+                     multilined>
+            <b-tag class="has-text-weight-bold" type="is-primary"
+                   style="margin-left: 10px;"
+                   v-model="isTargetToFomesMembers">테스트모드
+            </b-tag>
+          </b-tooltip>
+        </div>
+      </div>
+    </div>
+    <div class="contents">
+      <b-table
+        :data="result.answerRows"
+        :columns="result.questions"
+        :sticky-header="true"
+        style="height: 60vh"
+      ></b-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import request from '../../common/utils/http';
+
+export default {
+  name: 'TestFeedbackAggregation',
+  components: {
+  },
+  props:[
+    'betaTestId',
+    'missionId',
+    'betaTest',
+    'mission',
+  ],
+  computed: {
+    isTargetToFomesMembers: function() {
+      return this.betaTest.status === 'test';
+    }
+  },
+  data() {
+    return {
+      result: {
+        questions: [],
+        answerRows: [],
+      }
+    };
+  },
+  created() {
+    console.log('[TestFeedbackAggregation] created')
+    request.get('/api/beta-test/' + this.betaTestId + '/mission/' + this.missionId + '/feedback')
+    .then(res => {
+      console.log(res.data);
+      this.result.answerRows = res.data.answerRows;
+      this.result.questions = ["order"].concat(res.data.questions)
+        .map(question => {
+          return {
+            field: question,
+            label: question,
+            sticky: true
+        }
+      })
+      console.log(this.result);
+    }).catch(err => {
+      this.$root.showErrorToast('조회하는데 실패하였습니다.', err);
+    })
+  },
+  mounted() {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  margin: 0 auto !important;
+  width: 100% !important;
+}
+.head {
+  background: white;
+  padding: 1.5rem;
+  margin-bottom: 1rem;
+  margin-bottom: 1.5rem;
+}
+.contents {
+  margin: 0 auto !important;
+  width: 85% !important;
+}
+</style>
